4f12d3001df62d9accce49a035868d27c8a4d50a
moises <sistemas@surmotriz.com>
2016-11-11 20:57:03 +0100

    moises

--- apis.js
+++ apis.js
@@ -4,15 +4,14 @@ var bodyParser = require('body-parser');
 var oracledb = require('oracledb');
 var dbConfig = require('./dbconfig.js'); 
 
-var dconexion = {
+var conexion = {
     user          : dbConfig.user,
     password      : dbConfig.password,
     connectString : dbConfig.connectString     
 }
 
-
 router.get('/docs', function(req, res){
-    oracledb.getConnection(dconexion, function (err, conexion) {
+    oracledb.getConnection(conexion, function (err, conexion) {
         conexion.execute(
             "BEGIN PKG_ELECTRONICA.DOCS(:docs); END;",
             { 
@@ -29,6 +28,31 @@ router.get('/docs', function(req, res){
         );
     });
 });
+
+router.get('/fbc/:num_doc/:cla_doc/:co_cr/:exi_fra/:moneda/', function(req, res){
+    oracledb.getConnection(conexion, function(err, conexion){
+        conexion.execute(
+            "BEGIN PKG_ELECTRONICA.FBC(:num_doc,:cla_doc,:co_cr,:exi_fra,:moneda,:fbc); END;",
+            {
+                num_doc: { val: req.params.num_doc, dir:oracledb.BIND_IN },                
+                cla_doc: { val: req.params.cla_doc, type:oracledb.STRING },
+                co_cr:   { val: req.params.co_cr,   type:oracledb.STRING },
+                exi_fra: { val: req.params.exi_fra, type:oracledb.STRING },
+                moneda:  { val: req.params.moneda,  type:oracledb.STRING }, 
+                fbc:     { type: oracledb.CURSOR, dir: oracledb.BIND_OUT }
+            },
+            function(err, result){
+                result.outBinds.resultado.getRows(
+                    1,
+                    function(err, rows){
+                        res.contentType('application/json').send(JSON.stringify(rows));
+                    }
+                )
+            }
+        );      
+    });
+});
+
 
 
 /*
